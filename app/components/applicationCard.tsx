4df4f168fe036502b9fc40a9d5a90c05f11563bd
praveenR <praveen.r@deventure.co>
2024-12-23 11:58:40 +0530

    design change

--- app/components/applicationCard.tsx
+++ app/components/applicationCard.tsx
@@ -4,13 +4,14 @@ import { ApplicationList } from "../types/types";
 
 const customTheme: CustomFlowbiteTheme["card"] = {
   root: {
-    children: "flex h-full flex-col p-4 ",
+    children:
+      "flex h-full flex-col p-4 hover:pl-4 transition-all hover:border-l-4 hover:border-primary",
   },
 };
 
 function ApplicationCard(item: ApplicationList) {
   return (
-    <Card className="max-w-md" theme={customTheme}>
+    <Card className="max-w-md cursor-pointer" theme={customTheme}>
       <div className="flex gap-3 items-center">
         <Image src={item.imageUrl} alt={item.title} width={40} height={40} />
         <div className="font-extrabold text-lg bg-title-gradient bg-clip-text text-transparent">
