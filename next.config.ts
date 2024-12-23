acfaa666990d68f8318ffac24663bfde7ef58b51
praveenR <praveen.r@deventure.co>
2024-12-23 23:00:18 +0530

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,11 @@ const isProd = true;
 const repoName = "google-work-space"; // Replace with your GitHub repository name
 
 const nextConfig: NextConfig = {
-  output: "export",
   devIndicators: {
     appIsrStatus: false,
   },
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "",
-  trailingSlash: true,
 };
 
 export default nextConfig;
