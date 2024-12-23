d31d19b2192cbb0d1dd187bb4a0e0b11975d83c6
praveenR <praveen.r@deventure.co>
2024-12-23 22:38:27 +0530

    added gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = true;
+const repoName = "google-work-space"; // Replace with your GitHub repository name
+
 const nextConfig: NextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
